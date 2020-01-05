9e67cb01f106d4649973c064d51b15274d58381f
@bacardibryant <bacardi.bryant@outlook.com>
2020-01-05 11:16:15 -0600

    display books on the page. minor updates to application flow

--- js/app.js
+++ js/app.js
@@ -21,18 +21,28 @@ var bookDepot = {
 
     books:[],
 
+    getBookById:function(bookId){
+        var book = this.books.find(b=>b.id == bookId);
+        console.log(`Found ${book.title} by id.`);
+        return book;
+    },
+
     addBook:function(book){
         book.id = this.books.length + 1;
         this.books.push(book);
-        console.log(this.books);
+        console.log(`${book.title} has been added to the book collection.`);
     },
 
     removeBook:function(book){
-        for (let b in this.books){
-            if(this.books[b].id === book.id){
-                this.books.splice(this.books.findIndex(i => i.id === book.id),1);
-            }
-        }
+        var b = this.books.find(item => item === book);
+        this.books.splice(this.books.findIndex(i=>i.id === book.id),1);
+        console.log(`${book.title} has been removed from the book collection.`);
+    },
+
+    removeBookById:function(bookId){
+        var b = this.books.find(item => item.id == bookId);
+        this.books.splice(this.books.findIndex(i=>i.id == b.id),1);
+        console.log(`Book ${bookId} has been deleted by id.`);
         console.log(this.books);
     }
 
