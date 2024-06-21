81e980a21506c5f985d9a99b65d3fb1edce496b6
CIS Guru <bacardi.bryant@outlook.com>
2024-06-21 07:13:08 -0500

    see changes.md for revisions

--- v2/js/googleConfiguration.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-export class GoogleConfigurationOptions{
-    constructor(){
-        this.APIKeys = {
-            GoogleBooks:"AIzaSyA7vHRi6a8KG2XqQlLmWu3xCAkg_lpyP9k"
-        }
-    }
-}
